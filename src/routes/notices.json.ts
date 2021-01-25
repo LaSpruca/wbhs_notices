757e62b96bf8969ef9ee6e8bc14790301c28737e
Nathan Hare <me@laspruca.nz>
2021-01-25 20:43:22 +1300

    Using today

--- src/routes/notices.json.ts
+++ src/routes/notices.json.ts
@@ -42,13 +42,14 @@ type General = {
 }
 
 export const get = async (req, res, next) => {
-    // let today = new Date("11-03-2020");
+    let today = new Date();
 
     let params = new URLSearchParams();
     params.append("Command", "GetNotices");
     params.append("Key", "vtku");
-    // params.append("Date", `${today.getDay()}/${today.getMonth()}/${today.getFullYear()}`)
-    params.append("Date", `11/03/2020`)
+    params.append("Date", `${today.getDay()}/${today.getMonth()}/${today.getFullYear()}`)
+    // If you want to use a day with notices, for development purposes when there is no notices, comment out previous line and uncomment next line
+    // params.append("Date", `11/03/2020`)
 
     let noticesRequest = await axios.post("https://portal.wbhs.school.nz/api/api.php", params, {
         headers: {
