a30749dcc6a133eccd6e678990de035dace909f6
Nathan Hare <me@laspruca.nz>
2021-01-25 19:28:48 +1300

    Now displaying notices nicely

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,7 @@
     }
 
     type MeetingNotices = {
-        NumberMeetingRecords: string,
+        NumberMeetingRecords: number,
         Meeting: Array<Meeting>,
     }
 
@@ -40,8 +40,6 @@
     }
 
     export let notices: NoticesResults;
-
-    console.log(notices);
 </script>
 
 <script context="module" lang="ts">
@@ -56,22 +54,89 @@
 </script>
 
 <style lang="scss">
+  :global(*) {
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  :global(body) {
+    background: linear-gradient(to bottom, blue, cyan);
+    color: black;
+  }
+
+  .notice-wrapper {
+    padding: 30px;
+    margin: 10%;
+  }
+
+  .meeting .notice {
+    .info {
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 10px;
+      div {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        * {
+          width: fit-content;
+        }
+      }
+    }
+  }
+
+
+  .notice {
+    padding: 30px;
+    margin: 1rem 0;
+    background: linear-gradient(to bottom right, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.3));
+
+    border-radius: 30px;
+  }
 </style>
 
 <h1>WBHS Notices</h1>
 
-<div class="general">
+<h3>Meeting Notices</h3>
+<div class="meeting notice-wrapper">
+    {#if notices.MeetingNotices.NumberMeetingRecords !== 0 }
+        {#each notices.MeetingNotices.Meeting as notice}
+            <div class="notice">
+                <div class="info">
+                    <div>
+                        <p>{notice.Level}</p>
+                        <p>{notice.Subject}</p>
+                        <p>{notice.Teacher}</p>
+                    </div>
+                    <div class="e">
+                        <p>{notice.PlaceMeet}</p>
+                        <p>{notice.DateMeet}</p>
+                        <p>{notice.TimeMeet}</p>
+                    </div>
+                </div>
+                <p class="content">
+                    {notice.Body}
+                </p>
+            </div>
+        {/each}
+    {/if}
+</div>
+
+<h3>General Notices</h3>
+<div class="general notice-wrapper">
     {#if notices.GeneralNotices.NumberGeneralRecords !== 0 }
         {#each notices.GeneralNotices.General as notice}
-            <div class="info">
-                <p>{notice.Level}</p>
-                <p>{notice.Subject}</p>
-                <p>{notice.Teacher}</p>
+            <div class="notice">
+                <div class="info">
+                    <p>{notice.Level}</p>
+                    <p>{notice.Subject}</p>
+                    <p>{notice.Teacher}</p>
+                </div>
+                <p class="content">
+                    {notice.Body}
+                </p>
             </div>
-            <p class="content">
-                {notice.Body}
-            </p>
         {/each}
     {/if}
 </div>
